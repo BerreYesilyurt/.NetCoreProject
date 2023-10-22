1d6e32df61b904b773bc0462d6a0fea920661c0a
Berre Yesilyurt <berreyesilyurt23@gmail.com>
2023-10-22 21:37:33 +0300

    Area

---
+++ Core_Proje/Areas/Writer/Controllers/DefaultController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.Areas.Writer.Controllers
+{
+    public class DefaultController : Controller
+    {
+        [Area("Writer")] /*Hangi area ile çalışıldığının belirtlmesi için bir attribute*/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
